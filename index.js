ae721e469906db5b9da4927ab6800b7e333a00e2
Chris Gervang <gervangster@live.com>
2014-06-12 11:34:37 -0700

    access point command test

--- index.js
+++ index.js
@@ -1,4 +1,45 @@
 var Hapi = require('hapi');
+var sys  = require('sys');
+var fs   = require('fs');
+var exec = require('child_process').exec;
+var puts = function(error, stdout, stderr) { sys.puts(stdout) }
+
+//init access point
+exec("sudo ifdown wlan0", puts);
+
+var networkInterfaces = {
+	path: "/etc/network/interfaces",
+	content: {
+		connect: 
+			"auto lo\n\n
+			iface lo inet loopback\n
+			iface eth0 inet dhcp\n\n
+			auto wlan0\n
+			allow-hotplug wlan0\n
+			iface wlan0 inet dhcp\n
+			wpa-ssid \"Gervang Wireless\"\n
+			wpa-password \"cocoapunch\"",
+		
+		access: 
+			"auto lo\n\n
+			iface lo inet loopback\n
+			iface eth0 inet dhcp\n\n
+			iface wlan0 inet static\n
+			address 192.168.42.1\n
+			netmask 255.255.255.0"
+	}
+}
+
+fs.writeFile(networkInterfaces.path, networkInterfaces.content.access, function(err) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("networkInterfaces.content.access was saved!");
+        exec("ifconfig wlan0 192.168.42.1", puts);
+        exec("sudo service isc-dhcp-server start", puts);
+        exec("sudo service hostapd start", puts);
+    }
+});
 
 // Create a server with a host and port
 var server = Hapi.createServer('192.168.1.74', 8000);
@@ -10,11 +51,14 @@ var connect = function (request, reply) {
 	reply('connected!');
 }
 
-server.route([{
-    method: 'GET', path: '/{path*}', handler: {
+
+
+server.route([
+	{ method: 'GET', path: '/{path*}', handler: {
 	        directory: { path: './public', listing: false, index: true }
 	    }
-	},{ method: 'POST', path: '/connect', handler: connect }]);
+	},{ method: 'POST', path: '/connect', handler: connect }
+]);
 
 // Start the server
 server.start();
