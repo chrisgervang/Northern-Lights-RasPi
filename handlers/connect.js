4d789bdf7757c6147dd65970611ded075ae3244e
Chris Gervang <gervangster@live.com>
2014-07-05 23:47:35 -0700

    bug fix on networkInterfaces

--- handlers/connect.js
+++ handlers/connect.js
@@ -25,6 +25,27 @@ var initMining = function() {
 
 }
 
+var networkInterfaces = {
+	path: "/etc/network/interfaces",
+	content: {
+		connect: "auto lo\n\n" + 
+			"iface lo inet loopback\n" +
+			"iface eth0 inet dhcp\n\n" +
+			"auto wlan0\n" + 
+			"allow-hotplug wlan0\n" + 
+			"iface wlan0 inet dhcp\n" +
+			"wpa-ssid \"{ssid}\"\n" +
+			"wpa-psk \"{password}\"",
+		
+		access: "auto lo\n\n" +
+			"iface lo inet loopback\n" + 
+			"iface eth0 inet dhcp\n\n" + 
+			"iface wlan0 inet static\n" +
+			"address 192.168.42.1\n" + 
+			"netmask 255.255.255.0"
+	}
+}
+
 // Add the route
 var connect = function (request, reply) {
 	var credentials = request.payload;
--- index.js
+++ index.js
@@ -19,26 +19,7 @@ require("./check-online.js");
 //   console.log('ERROR!');
 // };
 
-var networkInterfaces = {
-	path: "/etc/network/interfaces",
-	content: {
-		connect: "auto lo\n\n" + 
-			"iface lo inet loopback\n" +
-			"iface eth0 inet dhcp\n\n" +
-			"auto wlan0\n" + 
-			"allow-hotplug wlan0\n" + 
-			"iface wlan0 inet dhcp\n" +
-			"wpa-ssid \"{ssid}\"\n" +
-			"wpa-psk \"{password}\"",
-		
-		access: "auto lo\n\n" +
-			"iface lo inet loopback\n" + 
-			"iface eth0 inet dhcp\n\n" + 
-			"iface wlan0 inet static\n" +
-			"address 192.168.42.1\n" + 
-			"netmask 255.255.255.0"
-	}
-}
+
 
 
 
